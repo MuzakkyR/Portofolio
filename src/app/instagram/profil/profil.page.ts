40913666849e209b647e1675c1d93d0b8e06b4ec
muzakky_r <muzakky_r@outlook.com>
2019-03-04 09:12:35 +0700

    update

--- src/app/instagram/profil/profil.page.ts
+++ src/app/instagram/profil/profil.page.ts
@@ -7,7 +7,59 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilPage implements OnInit {
 
-  constructor() { }
+  public storyArchieve:any = [];
+  public cta:any = [];
+  public timelineOption:any = [];
+  public footerIcon:any = [];
+  public postList:any = [];
+  
+
+  constructor() {
+    this.storyArchieve = [
+      {
+        thumbnail:"assets/instagram/add_story.svg",
+        name:"New"
+      },
+      {
+        thumbnail:"assets/instagram/kopi3.jpg",
+        name:"Quote"
+      },
+      {
+        thumbnail:"assets/instagram/kopi2.jpg",
+        name:"Manchester"
+      },
+    ]
+
+    this.cta = [
+      'Call',
+      'Email'
+    ]
+
+    this.timelineOption = [
+      "grid",
+      "phone-portrait",
+      "star",
+      "people" 
+    ]
+
+    this.footerIcon=[
+      "home",
+      "search",
+      "ios-add-circle-outline",
+      "heart-empty",
+    ]
+
+    this.postList = [
+      "assets/instagram/rose.jpg",
+      "assets/instagram/hands.jpg",
+      "assets/instagram/rose.jpg",
+      "assets/instagram/kopi1.jpg",
+      "assets/instagram/kopi2.jpg",
+      "assets/instagram/kopi3.jpg"
+      
+      
+    ]
+   }
 
   ngOnInit() {
   }
